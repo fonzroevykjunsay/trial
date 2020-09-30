78742fc3481ba9bc4c4db28ef26b35319b4b571d
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-10-01 01:45:49 +0800

    Add files via upload

---
+++ Project/Index.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div class="page">
+  <nav class="navbar">
+    <button class="navbar-item">Register</button>
+    <button class="navbar-item">Sign in</button>
+    <div class="anim"></div>
+</nav>
+  <div class="logo">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth tounge">
+        </span>
+      </figure>
+    <div class="logo-item">Cyanide and Happiness</div>
+    </div>
+    <div class="columns">Meet the characters
+    <div class="column-item">
+    <div class="item1">RED
+    <div class="red angry">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    </div>
+    <div class="item2">BROWN</div>
+    <div class="brown speechless">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item3">YELLOW</div>
+    <div class="yellow smile">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item4">GREEN</div>
+    <div class="green laugh">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
+    <div class="item5">BLUE</div>
+    <div class="blue sad">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
+</div>
+</div>
+<div class="columns">Watch our for our episodes!
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+</div>
+<ul class="bubbles">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+</body>
+</head>
+</html>
